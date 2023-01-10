0fd5a696664ac00fc72dc3f00c85dfc834a77df4
nestorpons <nestorpons@gmail.com>
2023-01-10 19:05:57 +0100

    two

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.15 <0.9.0;
+pragma solidity >=0.8.5 <0.9.0;
 
 contract TaskContract {
     uint256 counter;
@@ -19,24 +19,38 @@ contract TaskContract {
         autoCreate();
     }
     function hello() public pure returns(string memory){
-        return "Hola mundo";
+        return "Hola mundo 2";
     }
     function autoCreate() internal {
         create("T1", "Tarea 1");
+        create("T2", "Tarea 2");
+        create("T3", "Tarea 3");
+        create("T4", "Tarea 4");
+        create("T5", "Tarea 5");
+        create("T6", "Tarea 6");
     }
 
     function getLength() public view returns(uint256){
         return atasks.length;
     }
     function create(string memory name, string memory description) public {
-        uint id = counter++;
+        uint id = atasks.length;
         atasks.push(Task(id, name, description, 0, block.timestamp));
-        tasks[id] = Task(id, name, description, 0, block.timestamp);
     }
     function getTask(uint256 id) public view returns(string memory name, string memory description){
-        for (uint256 index = 0; index < atasks.length; index++) {
-            if (id == index) return (atasks[index].name, atasks[index].description);
+        return (tasks[id].name, tasks[id].description) ; 
+    }
+    function getaTask(uint _id) public view returns(uint id, string memory name, string memory description){
+        uint len = atasks.length;
+        uint ir = 0;  
+        for (uint i = 0; i < len ; i++) {
+            if (atasks[i].id == _id) return (atasks[i].id, atasks[i].name, atasks[i].description); 
+            ir = len-i-1;  
+            if (ir <= i ) break;
+            if (atasks[ir].id == _id) (atasks[i].id, atasks[ir].name, atasks[ir].description); 
         }
+
+        return (0, "","");
     }
     function getName(uint256 id)
         public
