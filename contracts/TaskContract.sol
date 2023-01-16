ac89b7b85d129f7dcbd7a513f2bdadb0b892f765
nestorpons <nestorpons@gmail.com>
2023-01-16 14:20:01 +0100

    frontend

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -1,93 +1,139 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.5 <0.9.0;
+pragma solidity ^0.8.7;
 
 contract TaskContract {
-    uint256 counter;
-
+    
     enum States{ DELETE, INITIAL, DONE, ERROR } States status;
-
+    
     struct Task {
         uint256 id;
         string name;
         string description;
         States status; // 0 delete 1 Initial 2 Done
-        uint256 createAt;
+        uint256 create;
+        uint256 expire;
     }
 
-    mapping(uint256 => Task) public tasks;
-    Task[] atasks; 
-
+    struct Self {
+        uint256 counter;
+        uint addTime; 
+        Task[] tasks; 
+    }
+    
+    Self self;
+    
     constructor(){
-        autoCreate();
-    }
-   
-    function autoCreate() internal {
-        createTask("", "");
-        createTask("T1", "Tarea 1");
-        createTask("T2", "Tarea 2");
-        createTask("T3", "Tarea 3");
-        createTask("T4", "Tarea 4");
-        createTask("T5", "Tarea 5");
-        createTask("T6", "Tarea 6");
+        self.addTime = 7 days; 
+        createTask("", ""); //Record trash
     }
 
     function getLength() public view returns(uint256){
-        return atasks.length;
+        return self.tasks.length;
     }
+
     function createTask(string memory name, string memory description) public {
-        uint id = atasks.length;
-        atasks.push(Task(id, name, description, States.INITIAL, block.timestamp));
+        uint id = self.tasks.length;
+        uint expire = block.timestamp + self.addTime; 
+        self.tasks.push(Task(id, name, description, States.INITIAL, block.timestamp, expire));
     }
+
     function updateName(uint id, string memory value) public{
         uint i = getIndex(id);
-        atasks[i].name = value;
+        self.tasks[i].name = value;
     }
+
     function updateDescription(uint id, string memory value) public{
         uint i = getIndex(id);
-        atasks[i].description = value;
+        self.tasks[i].description = value;
     }
+
     function deleteTask(uint id) public {
         updateStatus(id, 0);
     }
+
     function resetTask(uint id) public {
         updateStatus(id, 1);
     }
+
     function doneTask(uint id) public{
         updateStatus(id, 2);
     }
+
     function updateStatus(uint id, uint8 idStatus ) internal {
         uint i = getIndex(id);
-        atasks[i].status = 
+        self.tasks[i].status = 
             idStatus == 0 ? States.DELETE : 
             idStatus == 1 ? States.INITIAL :
             idStatus == 2 ? States.DONE :
             States.ERROR ;
-        if(atasks[i].status == States.ERROR){
+        if(self.tasks[i].status == States.ERROR){
             revert("ERROR: Index out bound!");
         }
     }
+
     function getTask( uint id) public view returns(Task memory){
-        uint len = atasks.length;
+        uint len = self.tasks.length;
         uint ir = 0;  
         for (uint i = 0; i < len ; i++) {
-            if (atasks[i].id == id) return atasks[i]; 
+            if (self.tasks[i].id == id) return self.tasks[i]; 
             ir = len-i-1;  
             if (ir <= i ) break;
-            if (atasks[ir].id == id) return atasks[i]; 
+            if (self.tasks[ir].id == id) return self.tasks[i]; 
         }
 
-        return atasks[0];
+        return self.tasks[0];
     }
+
     function getIndex(uint id) internal view returns(uint){
-        uint len = atasks.length;
+        uint len = self.tasks.length;
         uint ir = 0;  
         for (uint i = 0; i < len ; i++) {
-            if (atasks[i].id == id) return i; 
+            if (self.tasks[i].id == id) return i; 
             ir = len-i-1;  
             if (ir <= i ) break;
-            if (atasks[ir].id == id) return ir; 
+            if (self.tasks[ir].id == id) return ir; 
         }
 
         return 0;
+    }
+
+    function getStatus(uint id) public view returns(uint8) {
+        uint i = getIndex(id);
+        uint8 respond =
+            self.tasks[i].status == States.DELETE ? 0 : 
+            self.tasks[i].status == States.INITIAL ? 1:
+            2 ;
+        return respond;
+    }
+    function getExpire(uint id) public view returns(uint){
+        uint i = getIndex(id);
+        return self.tasks[i].expire;
+    }
+    
+    function changeTimeExpire(uint id, uint numDays) public{
+        uint i = getIndex(id);
+        if (numDays == 1){
+            self.addTime = 1 days;
+        } else if (numDays == 2){
+            self.addTime = 2 days;
+        } else if (numDays == 3){
+            self.addTime = 3 days;
+        } else if (numDays == 4){
+            self.addTime = 4 days;
+        } else if (numDays == 5){
+            self.addTime = 5 days;
+        } else if (numDays == 6){
+            self.addTime = 6 days;
+        } else if (numDays == 7){
+            self.addTime = 7 days;
+        } else if (numDays == 14){
+            self.addTime = 1 weeks;
+        } else if (numDays == 30){
+            self.addTime = 2 weeks;
+        } else {
+            revert("You must select one of the following items \n 1,2,3,4,5,6,7,14,30");
+        }
+
+        self.tasks[i].expire = self.tasks[1].create + self.addTime; 
     }
 }
--- test/task.test.sol
+++ test/task.test.sol
@@ -3,19 +3,64 @@ pragma solidity >=0.8.5 <0.9.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "truffle/Console.sol";
-
-import "../lib/solidity-tester.sol";
 import "../contracts/TaskContract.sol";
 
 contract TestTask {
-    TaskContract tsk;
-
-    function beforeAll() public{
+    TaskContract tsk;           
+    constructor(){
         tsk = new TaskContract();
     }
-    
-    function testNameIsString() public {
-        Assert.isEmpty("", "Test string not is empty!");
+
+    function beforeAll() public {
+        tsk.createTask("T1", "Tarea 1");
+        tsk.createTask("T2", "Tarea 2");
+        tsk.createTask("T3", "Tarea 3");
+        tsk.createTask("T4", "Tarea 4");
+        tsk.createTask("T5", "Tarea 5");
+        tsk.createTask("T6", "Tarea 6");
+    }
+
+    function testCreateTask() public {
+        uint len = tsk.getLength();
+        Assert.equal(len, 7, "testCreateTask 1");
+    }
+
+    function testDeleteTask() public {
+        tsk.deleteTask(1);
+        uint st = tsk.getStatus(1);
+        Assert.equal(st, 0, "testDeleteTask 1");
+    }
+
+    function testChangeDefaultTime() public {
+        tsk.changeTimeExpire(1, 2);
+        uint change = tsk.getExpire(1);
+        Assert.isNotZero(change,"testChangeDefaultTime 1");
+        tsk.changeTimeExpire(1, 30);
+        uint change2 = tsk.getExpire(1);
+        Assert.notEqual(change, change2, "testChangeDefaultTime 2");
+        string memory len = uint2str(change);
+        Assert.isBelow(bytes(len).length,12,string.concat("testChangeDefaultTime 3:", len));
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+        k  = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
